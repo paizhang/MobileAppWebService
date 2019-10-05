04b32d80844a0f78f76b2da959c294c1ea35b1c5
paizhang <zhangpai91@gmail.com>
2019-10-05 15:44:40 -0700

    Add create, update, get and delete demo methods for UserController class.

--- mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,10 +1,33 @@
 package com.appdeveloperblog.app.ws.ui.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("users")
 public class UserController {
 	
+	@GetMapping
+	public String getUser() {
+		return "get user was called!";
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "Create user was called!";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "Update user was called!";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "Delete user was called!";
+	}
 }
