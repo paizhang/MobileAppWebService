dc820aa06b89d861c32200897e4380ac6bfd540c
paizhang <zhangpai91@gmail.com>
2019-10-19 22:36:08 -0700

    Add encryption for user password and web service endpoint.

--- mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.appdeveloperblog.app.ws.service.impl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.appdeveloperblog.app.ws.UserRepository;
 import com.appdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appdeveloperblog.app.ws.service.UserService;
+import com.appdeveloperblog.app.ws.shared.Utils;
 import com.appdeveloperblog.app.ws.shared.dto.UserDto;
 
 @Service
@@ -15,13 +17,26 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	Utils utils;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@Override 
 	public UserDto createUser(UserDto userDto) {
+		UserEntity storedUserDetails = userRepository.findUserByEmail(userDto.getEmail());
+		if (storedUserDetails != null) {
+			throw new RuntimeException("Record already exists!");
+		}
+		
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(userDto, userEntity);
 		
-		userEntity.setEncryptedPassword("test");
-		userEntity.setUserId("TestUserId");
+		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
+		
+		String userId = utils.generateUserId(30);
+		userEntity.setUserId(userId);
 		
 		UserEntity storedEntity = userRepository.save(userEntity);
 		
---
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/shared/Utils.java
@@ -0,0 +1,27 @@
+package com.appdeveloperblog.app.ws.shared;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utils {
+	
+	private final Random RANDOM = new SecureRandom();
+	private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	
+	public String generateUserId(int length) {
+		return generateRandomString(length);
+	}
+	
+	private String generateRandomString(int length) {
+		StringBuilder sb = new StringBuilder(length);
+		
+		for (int i = 0; i < length; i++) {
+			sb.append(ALPHABET.charAt(RANDOM.nextInt(length)));
+		}
+		
+		return sb.toString();
+	}
+}
